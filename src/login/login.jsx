1666b23b994e8fb3c82d101fa81679c3494af767
John <johnnephiolson@gmail.com>
2025-02-24 08:53:36 -0700

    Modified login.jsx and added Authenticated.jsx class to show menu when authenticated

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 
-export function Login() {
+import { Unauthenticated } from './unauthenticated';
+import { Authenticated } from './Authenticated';
+import { AuthState } from './AuthState'
+
+export function Login({ userName, authState, onAuthChange}) {
     return (
         <main>
-        <h2>Welcome!</h2>
+        {authState !== AuthState.Unknown && <h2>Welcome!</h2>}
+        {authState === AuthState.Authenticated && (
+          <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />
+        )}
+        {authState === AuthState.Unauthenticated && (
+          <Unauthenticated
+            userName={userName}
+            onLogin={(loginUserName) => {
+              onAuthChange(loginUserName, AuthState.Authenticated)
+            }}
+          />
+        )}
+        /*Code below not necessary*/
         <p>Login to begin</p>
         <form className="make-column" action="create.html" method="get">
           <input type="text" id="name" className="add-margin" placeholder="Your name here" />
